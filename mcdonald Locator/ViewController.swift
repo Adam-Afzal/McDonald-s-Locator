e73d0a1ee68f231958048f2c6f015cf3a67e147f
kyles7 <ks@Kyle.local>
2018-02-22 14:21:03 +0000

    Initial Commit

---
+++ mcdonald Locator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mcdonald Locator
+//
+//  Created by Kyle on 22/02/2018.
+//  Copyright © 2018 Kyle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
